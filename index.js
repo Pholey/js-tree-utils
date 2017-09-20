138bf92bc2d69db88f6813e311072aa1a6aea00a
Cassidy Bridges <cassidy.bridges@gmail.com>
2017-09-20 09:33:19 -0700

    export node

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = {
   BinarySearchTree: require('./src/binarySearchTree.js'),
-  AvlTree: require('./src/avlTree.js')
+  AvlTree: require('./src/avlTree.js'),
+  Node: require("./src/node"),
 }
